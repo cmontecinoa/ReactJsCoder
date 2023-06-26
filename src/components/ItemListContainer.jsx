4ba562c0520caee3af8b79876b9bd96e3046d719
Camilo Montecino A <camilo.montecino.a@gmail.com>
2023-06-25 23:56:08 -0400

    Primer uso de Firebase y Firestore, Envio de Proyecto Final

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,38 +1,68 @@
-import { useState, useEffect} from "react";
+import { useState , useEffect } from "react";
 // import ItemCount from "./ItemCount";
-import ItemList from "./ItemList";
-import productos from "./json/productos.json";
-import { useParams } from "react-router-dom";
+// import ItemList from "./ItemList";
+// import productos from "./json/productos.json";
+// import { useParams } from "react-router-dom";
+
+//Firestore
+import { doc, getDoc, getFirestore} from "firebase/firestore";
 
 
 const ItemListContainer = () => {
 
 
     const [items, setItems] = useState([]);
-    const {id} = useParams();
+    // const {id} = useParams();
+
+//Accediento a los productos vía json
+
+    // useEffect(() => {
+    //     const promesa = new Promise((resolve) => {
+    //         setTimeout(() => {
+    //             resolve( id ? productos.filter(item => item.categoria === id) : productos)
+
+    //         }, 1000);
+    //     });
+
+    //     promesa.then(data => {
+    //         console.log(data);
+    //         setItems(data);
+    //     })
+    // }, [id]);
+
+//Accediendo a los productos vía firestore
 
     useEffect(() => {
-        const promesa = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve( id ? productos.filter(item => item.categoria === id) : productos)
+        const db = getFirestore();
 
-            }, 1000);
-        });
-
-        promesa.then(data => {
-            console.log(data);
-            setItems(data);
+        const producto = doc(db, "items", "wNxtCoMdwVEO2HQxl3CC");
+        
+        getDoc(producto).then(resultado => {
+            if(resultado.exists()){
+                setItems({id:resultado.id, ...resultado.data()})
+            }else{
+                console.error("No se ha encontrado el producto")
+            }
+            // console.log("ID Producto: " + resultado.id);
+            // console.log(resultado.data());
         })
-    }, [id]);
+
+    })
 
 
 
     return (
         <div className="container my-5">
             <div className="row">
-                
-                    <ItemList productos ={items} />
-                    {/* <ItemCount stock={10} /> */}
+                <div className="col-md-4">
+                    <img src={items.imagen} alt={items.titulo} className="img-fluid"/>
+                </div>
+                <div className="col-md-4">
+                    <h2>{items.titulo}</h2>
+                    <p>${items.precio}</p>
+                    
+                </div>
+                     {/* <ItemList productos ={items} />  */}
 
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0HYvOVmjp_DnRKJcjhQPJsPsTESXR0rw",
+  authDomain: "bullsmoke-ecommerce-43220.firebaseapp.com",
+  projectId: "bullsmoke-ecommerce-43220",
+  storageBucket: "bullsmoke-ecommerce-43220.appspot.com",
+  messagingSenderId: "997721860341",
+  appId: "1:997721860341:web:a986b89e18014a72cb3dbc"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
